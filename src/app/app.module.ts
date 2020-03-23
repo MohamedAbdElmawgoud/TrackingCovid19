71829603ca642bb6ef5eb98b75992f73a3b7403f
Mohamed Abd ELmawgoud <49039674+MohamedAbdElmawgoud@users.noreply.github.com>
2020-03-23 12:11:45 +0200

    map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,12 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GogleMapComponent } from "src/app/gogle-map/gogle-map.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,GogleMapComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,],
   providers: [
     StatusBar,
     SplashScreen,
