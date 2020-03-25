6d9fbc831deb43cd723cfa79acff7a3dd6e13b4b
nader medhet <nadermedhet142@gmail.com>
2020-03-25 11:06:49 +0200

    fix maps issue

--- src/app/gogle-map/gogle-map.component.ts
+++ src/app/gogle-map/gogle-map.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-import { ToastController } from '@ionic/angular';
-
+import { ToastController, Platform } from '@ionic/angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment,
+  GoogleMapsAnimation,
+  MyLocation
+} from '@ionic-native/google-maps';
 declare const google;
 const icon = {
   url: 'assets/icon/marker.svg', // image url
@@ -14,157 +25,213 @@ const icon = {
   styleUrls: ['./gogle-map.component.scss'],
 })
 export class GogleMapComponent implements OnInit {
-  @ViewChild("map" , {static : true}) mapElement;
-  map: any;
-  lat:number;
-  lng:number;
-  polylineOptions;
-  destination =[];
-  
+
+  map: GoogleMap;
+  address:string;
  
- 
-  
-  suppressMarkers: true;
   constructor(
-    private geo: Geolocation
-    ,public toastController: ToastController) {}
-    
-    // async presentToast(title) {
-    //   const toast = await this.toastController.create({
-    //     message: title,
-    //     duration: 3000
-    //   });
-    //   toast.present();
-    // }
- getMyLocation() {
-  this.geo.getCurrentPosition({
-    
-    enableHighAccuracy: true
-    
-    
-  }).then(location => {
-   
-    this.lat = location.coords.latitude;
-    this.lng = location.coords.longitude;
-   
-    this.destination = [
-      {lat: this.lat, lng: this.lng},
-     
-      
-    ];
-    this.initMap(this.lat,this.lng);
-    this.cretePolyLine(this.destination ,'#FF0000');
-    
-  })
-   setInterval(()=>{
-    this.geo.getCurrentPosition({
-      enableHighAccuracy: true
-    }).then(location => {
-      
-      this.lat = location.coords.latitude;
-      this.lng = location.coords.longitude;
-    let point= this.addPoint(this.lat,this.lng);
-    let points = this.addPoint( point.lato,point.lono);
-    let pointss = this.addPoint( points.lato,points.lono);    
-    let test = [];
-      test.push(point,points,pointss);
-
-      this.initMap(this.lat,this.lng,);
-        this.destination = [
-          {lat: this.lat, lng: this.lng},
-          {lat: test[0].lato, lng: test[0].lono},
-          {lat: test[1].lato, lng: test[1].lono},
-         
-        ];
-      
-      this.cretePolyLine(this.destination,'#FF0000');
-      this.destination=[
-        {lat: test[2].lato, lng: test[2].lono},
-        {lat: test[2].lato+(0.000009000009*10), lng: test[2].lono+(0.000009000009*10)},
-      ]
-      this.cretePolyLine(this.destination,'#00FFA3');
-      this.destination = [
-       
-        {lat: test[2].lato+(0.000009000009*50), lng: test[0].lono+(0.000009000009*50)},
-        {lat: test[1].lato+(0.000009000009*50), lng: test[1].lono+(0.000009000009*50)},
-       
-      ];
-      this.cretePolyLine(this.destination,'#FFB200');
-
-      this.destination=[
-        {lat: test[1].lato, lng: test[2].lono},
-        {lat: test[2].lato+(0.000009000009*80), lng: test[2].lono+(0.000009000009*80)},
-      ]
-      this.cretePolyLine(this.destination,'#00BCFF');
-      this.destination = [
-       
-        {lat: test[2].lato+(0.000009000009*200), lng: test[2].lono+(0.000009000009*100)},
-        {lat: test[1].lato+(0.000009000009*10), lng: test[1].lono+(0.000009000009*10)},
-       
-      ];
-      this.cretePolyLine(this.destination,'#194B11');
-      // this.presentToast(this.lat)
-      
-     
-    })},
-    10000
-  )
-   
-    
+    public toastCtrl: ToastController,
+    private platform: Platform
+    ) { }
+ 
+  ngOnInit() {
+    // Since ngOnInit() is executed before `deviceready` event,
+    // you have to wait the event.
+    this.platform.ready();
+    this.loadMap();
   }
-
-  ngOnInit(){
-    
-    this.getMyLocation();
+ 
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      // camera: {
+      //   target: {
+      //     lat: 43.0741704,
+      //     lng: -89.3809802
+      //   },
+      //   zoom: 18,
+      //   tilt: 30
+      // }
+    });
+    this.goToMyLocation();
   }
-  addPoint(lat,lng){
-   
-    let R=6378137
-    
-    //  //offsets in meters
-    //  let dn = 10 
-    //  let de = 10
-    
-    //  //Coordinate offsets in radians
-    //  let dLat = dn / R ;
-    //  let dLon = de / (R * Math.acos(3.14 * dLat / 180));
-   
-     //OffsetPosition, decimal degrees
-// ==(0.000009000009*10)
-
-     let latO = lat +(0.000009000009*10);
-     let lonO = lng +(0.000009000009*10) ;
-     return {lato: latO ,lono: lonO}
-  }
-
-  initMap(lat,lng,){
-    
-   
-    let coords = new google.maps.LatLng(lat,lng);
-    let mapOptions= {
-      center: coords,
-      zoom: 18,
-      mapTypeId: (<any>google).maps.MapTypeId.ROADMAP
-    }
-    
-    this.map = new (<any>google).maps.Map(this.mapElement.nativeElement, mapOptions)
-
-    let marker = new (<any>google).maps.Marker({
-      map: this.map,
-      position: coords,
-      icon: icon, 
-      title: 'goda!',
-      
-      // animation: window['google'].maps.Animation.DROP,
-      
+ 
+ 
+  goToMyLocation(){
+    this.map.clear();
+ 
+    // Get the location of you
+    this.map.getMyLocation().then((location: MyLocation) => {
+      console.log(JSON.stringify(location, null ,2));
+ 
+      // Move the map camera to the location with animation
+      this.map.animateCamera({
+        target: location.latLng,
+        zoom: 17,
+        duration: 5000
+      });
+ 
+      //add a marker
+      let marker: Marker = this.map.addMarkerSync({
+        title: '@ionic-native/google-maps plugin!',
+        snippet: 'This plugin is awesome!',
+        position: location.latLng,
+        animation: GoogleMapsAnimation.BOUNCE
+      });
+ 
+      //show the infoWindow
+      marker.showInfoWindow();
+ 
+      //If clicked it, display the alert
+      marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+        this.showToast('clicked!');
+      });
+ 
+      this.map.on(GoogleMapsEvent.MAP_READY).subscribe(
+        (data) => {
+            console.log("Click MAP",data);
+        }
+      );
     })
-    var infoWindow = new google.maps.InfoWindow({
-      content: 'Goda address'
+    .catch(err => {
+      //this.loading.dismiss();
+      this.showToast(err.error_message);
     });
+  }
+ 
+  async showToast(message: string) {
+    let toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      position: 'middle'
+    });
+    toast.present();
+  }
+//  getMyLocation() {
+//   this.geo.getCurrentPosition({
+    
+//     enableHighAccuracy: true
+    
+    
+//   }).then(location => {
+   
+//     this.lat = location.coords.latitude;
+//     this.lng = location.coords.longitude;
+   
+//     this.destination = [
+//       {lat: this.lat, lng: this.lng},
+     
+      
+//     ];
+//     this.initMap(this.lat,this.lng);
+//     this.cretePolyLine(this.destination ,'#FF0000');
+    
+//   })
+//    setInterval(()=>{
+//     this.geo.getCurrentPosition({
+//       enableHighAccuracy: true
+//     }).then(location => {
+      
+//       this.lat = location.coords.latitude;
+//       this.lng = location.coords.longitude;
+//     let point= this.addPoint(this.lat,this.lng);
+//     let points = this.addPoint( point.lato,point.lono);
+//     let pointss = this.addPoint( points.lato,points.lono);    
+//     let test = [];
+//       test.push(point,points,pointss);
 
-    google.maps.event.addListener(marker, 'click', function () {
-      infoWindow.open(this.map, marker);
-    });
+//       this.initMap(this.lat,this.lng,);
+//         this.destination = [
+//           {lat: this.lat, lng: this.lng},
+//           {lat: test[0].lato, lng: test[0].lono},
+//           {lat: test[1].lato, lng: test[1].lono},
+         
+//         ];
+      
+//       this.cretePolyLine(this.destination,'#FF0000');
+//       this.destination=[
+//         {lat: test[2].lato, lng: test[2].lono},
+//         {lat: test[2].lato+(0.000009000009*10), lng: test[2].lono+(0.000009000009*10)},
+//       ]
+//       this.cretePolyLine(this.destination,'#00FFA3');
+//       this.destination = [
+       
+//         {lat: test[2].lato+(0.000009000009*50), lng: test[0].lono+(0.000009000009*50)},
+//         {lat: test[1].lato+(0.000009000009*50), lng: test[1].lono+(0.000009000009*50)},
+       
+//       ];
+//       this.cretePolyLine(this.destination,'#FFB200');
+
+//       this.destination=[
+//         {lat: test[1].lato, lng: test[2].lono},
+//         {lat: test[2].lato+(0.000009000009*80), lng: test[2].lono+(0.000009000009*80)},
+//       ]
+//       this.cretePolyLine(this.destination,'#00BCFF');
+//       this.destination = [
+       
+//         {lat: test[2].lato+(0.000009000009*200), lng: test[2].lono+(0.000009000009*100)},
+//         {lat: test[1].lato+(0.000009000009*10), lng: test[1].lono+(0.000009000009*10)},
+       
+//       ];
+//       this.cretePolyLine(this.destination,'#194B11');
+//       // this.presentToast(this.lat)
+      
+     
+//     })},
+//     10000
+//   )
+   
+    
+//   }
+
+
+//   addPoint(lat,lng){
+   
+//     let R=6378137
+    
+//     //  //offsets in meters
+//     //  let dn = 10 
+//     //  let de = 10
+    
+//     //  //Coordinate offsets in radians
+//     //  let dLat = dn / R ;
+//     //  let dLon = de / (R * Math.acos(3.14 * dLat / 180));
+   
+//      //OffsetPosition, decimal degrees
+// // ==(0.000009000009*10)
+
+//      let latO = lat +(0.000009000009*10);
+//      let lonO = lng +(0.000009000009*10) ;
+//      return {lato: latO ,lono: lonO}
+//   }
+
+//   initMap(lat,lng,){
+    
+   
+//     let coords = new google.maps.LatLng(lat,lng);
+//     let mapOptions= {
+//       center: coords,
+//       zoom: 18,
+//       mapTypeId: (<any>google).maps.MapTypeId.ROADMAP
+//     }
+    
+//     this.map = new (<any>google).maps.Map(this.mapElement.nativeElement, mapOptions)
+
+//     let marker = new (<any>google).maps.Marker({
+//       map: this.map,
+//       position: coords,
+//       icon: icon, 
+//       title: 'goda!',
+      
+//       // animation: window['google'].maps.Animation.DROP,
+      
+//     })
+//     var infoWindow = new google.maps.InfoWindow({
+//       content: 'Goda address'
+//     });
+
+//     google.maps.event.addListener(marker, 'click', function () {
+//       infoWindow.open(this.map, marker);
+//     });
     
    
  
@@ -172,17 +239,17 @@ export class GogleMapComponent implements OnInit {
   
 
   
-  }
-  cretePolyLine(destination ,color){
-    this.polylineOptions = new google.maps.Polyline({
-      path: destination,
-      geodesic: true,
-      strokeColor: color,
-      strokeOpacity: 1.0,
-      strokeWeight: 3
-    });
-    this.polylineOptions.setMap(this.map);
-  }
+//   }
+//   cretePolyLine(destination ,color){
+//     this.polylineOptions = new google.maps.Polyline({
+//       path: destination,
+//       geodesic: true,
+//       strokeColor: color,
+//       strokeOpacity: 1.0,
+//       strokeWeight: 3
+//     });
+//     this.polylineOptions.setMap(this.map);
+//   }
 
 
 }
