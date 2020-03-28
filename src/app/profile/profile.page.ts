4067545c2258d50b78ed4b36919831d247eb02b2
nader medhet <nadermedhet142@gmail.com>
2020-03-28 18:10:14 +0200

    fix profile erorr

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -15,23 +15,23 @@ export class ProfilePage implements OnInit {
   constructor(private api: ApiService, private storage: Storage) { }
 
   async ngOnInit() {
-    let user = await this.storage.get('user');
-
+    let user = (await this.storage.get('user'));
+    user = user? user : {};
     this.user = await this.api.getUser(user.id);
-    console.log(this.user);
+
 
     this.status = await this.api.getUpdateStatus(
       {
         userId: user.id,
         latitude: 30.1, //Double
         longitude: 31.0, //Double
-        colorId: this.user.color.id
+        colorId: this.user ? this.user.color.id : 0
       }
     );
-    await this.storage.set('user' , this.user)
+    await this.storage.set('user', this.user)
     console.log('status is ', this.status)
     // this.img = await this.api.getImgUser(this.user.imageName);
-    if (!this.user.gender) {
+    if (this.user && !this.user.gender) {
       this.gender = 'Male'
     }
     else {
